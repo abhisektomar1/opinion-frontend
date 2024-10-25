cef5dc734833dd323a0d5b4a9dce96909627d1d9
Abhishek <abhisektomar1@gmail.com>
2024-10-25 20:19:05 +0530

    hero section

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['aceternity.com',"assets.aceternity.com","probo.in"],
+    },
+};
+
+export default nextConfig;
